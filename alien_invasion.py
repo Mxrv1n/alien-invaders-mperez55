995b3a4e5fb023fc3d30bc9cdbc9e51a70962897
Marvin P <MarvinPerez1503@outlook.com>
2026-04-11 02:46:09 -0400

    background color changed

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,6 +12,7 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
+        self.bg_color = (230, 230, 230)
     def run_game(self):
         """Start the main loop for the game."""
         while True:
@@ -20,6 +21,9 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             
+            """"redraw the screen during each pass through the loop."""
+            self.screen.fill(self.bg_color)
+
             """Make the most recently drawn screen visible."""
             pygame.display.flip()
             self.clock.tick(60)  # Limit the frame rate to 60 FPS
