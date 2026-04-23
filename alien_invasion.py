0e8af6c65d4abc45ccdd9e45dcb7dffc72ecdfc9
Marvin P <MarvinPerez1503@outlook.com>
2026-04-22 22:26:46 -0400

    changes a couple settings implemented life counter

--- alien_invasion.py
+++ alien_invasion.py
@@ -126,6 +126,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.sb.prep_score()
             self.sb.prep_level()
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -223,6 +224,7 @@ class AlienInvasion:
         #decrement ships_left.
         if self.stats.ships_left > 0:
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ class Settings:
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (0, 0, 0)
 
         #ship settings
         self.ship_limit = 3
@@ -14,7 +14,7 @@ class Settings:
         # Bullet settings
         self.bullet_width = 5
         self.bullet_height = 20
-        self.bullet_color = (60, 60, 60)
+        self.bullet_color = (255, 0, 0)
         self.bullets_allowed = 5
 
         # Alien settings
